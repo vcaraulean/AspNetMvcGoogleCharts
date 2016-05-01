e6b8cbcc04ce3f1653080166a3169ddc27775310
Valeriu Caraulean <valeriu@caraulean.com>
2016-05-01 16:44:09 +0200

    Initial import. Empty project with updated nuget packages

---
+++ MvcCharts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCharts.MvcApplication" Language="C#" %>
---
+++ MvcCharts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcCharts.Startup))]
+namespace MvcCharts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
